98c854c334aab0bdb5281b9a27daf389c97e2323
Max Kolbin <kolbinmaxim013@gmail.com>
2024-01-07 18:16:51 +0200

    change-button

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const nav = document.querySelector('.nav-icon-middle');
 const navMobil = document.querySelector('.nav-mobil');
 
 navIcon.addEventListener('click', function () {
-	this.classList.toggle('nav-icon');
-	nav.classList.toggle('nav-icon--active');
+	this.classList.toggle('nav-icon--active');
+	nav.classList.toggle('nav-icon-middle--active');
 	navMobil.classList.toggle('nav-mobil--active');
 });
